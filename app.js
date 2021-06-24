f4a0b6d57f7c0c8968b9d0de8850bdab3013f9bd
aseelalsaqer <aseel_fawwaz@yahoo.com>
2021-06-24 20:44:46 +0300

    problem6 solution

--- app.js
+++ app.js
@@ -120,15 +120,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
-    // let q = 1;
-    // for (let i = 0; i < 3; i++) {
-    //     q = multiply(q, multArr[i])[0];
-    // }
-    // return [q ,'The numbers ' + multArr +' have a product of '+ q +'.'];
+    let q = 1;
+    for (let i = 0; i < 3; i++) {
+        q = multiply(q, multArr[i])[0];
+    }
+    return [q ,'The numbers ' + multArr +' have a product of '+ q +'.'];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
@@ -159,14 +159,14 @@ Test this function by hand in the console to get it working, and when you think 
 let testDynamicArray = [1, 2, 3, 4, 5]; //eslint-disable-line
 
 function multiplyAnyArray(dynamicArray) { //eslint-disable-line
-    // let q = 1;
-    // for (let i = 0; i < dynamicArray.length; i++) {
-    //     q = multiply(q, dynamicArray[i])[0];
-    // }
-    // return [q ,'The numbers ' + dynamicArray +' have a product of '+ q +'.'];
+    let q = 1;
+    for (let i = 0; i < dynamicArray.length; i++) {
+        q = multiply(q, dynamicArray[i])[0];
+    }
+    return [q ,'The numbers ' + dynamicArray +' have a product of '+ q +'.'];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyAnyArray(testDynamicArray);
+testMultiplyAnyArray(testDynamicArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
